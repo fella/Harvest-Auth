0e97dd024c35f2fddd65d792483c247b707a191c
Dave Spurr <quicksandee@gmail.com>
2025-05-08 15:34:02 -0700

    Update index.ts
    Remove "Hello World"

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.doc('/openapi.json', {
   }
 })
 // Protected route
-app.get('/', (c) => c.text('Hello World'))
+// app.get('/', (c) => c.text('Hello World'))
 
 const REQUIRED_ROLE = 'Admin'
 
